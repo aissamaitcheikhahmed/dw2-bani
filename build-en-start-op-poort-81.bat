4eeea2ff2b31e9f0b5ff3b46e87d5921a1767c83
aissamaitcheikhahmed <aissam.aitcheikhahmed@student.hogent.be>
2024-05-20 15:24:44 +0200

    final fix

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -1,8 +1,7 @@
 @echo off
 docker build -t demo-manual-site-image .
 
-docker stop demo-manual-site-container
-docker rm demo-manual-site-container
+
 
 docker run -d -p 81:80 --name demo-manual-site-container demo-manual-site-image
 
