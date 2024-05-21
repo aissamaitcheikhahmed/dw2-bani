077f1ad25feaed81ae80e75361971c6ea8ef92d5
aissamaitcheikhahmed <aissam.aitcheikhahmed@student.hogent.be>
2024-05-21 22:56:23 +0200

    adocker run commando toegevoegd

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -2,3 +2,5 @@ docker build -t demo-manual-site-image .
 docker stop demo-manual-site-container
 docker rm demo-manual-site-container
 
+docker run -d -p 81:80 --name demo-manual-site-container demo-manual-site-image
+
